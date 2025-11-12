98bf9e4ce498c616bc33c02ba981cda7fdb16fff
Erik Behrends <behrends@users.noreply.github.com>
2025-11-12 16:31:43 +0100

    Korrekte lokale Speicherung der Auth-Daten

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,6 +6,16 @@ import {
 import { getFirestore } from 'firebase/firestore';
 import * as SecureStore from 'expo-secure-store';
 
+const sanitizeKey = (key) => key.replace(/[^a-zA-Z0-9._-]/g, '_');
+
+const secureStorePersistence = {
+  getItem: (key) => SecureStore.getItemAsync(sanitizeKey(key)),
+  setItem: (key, value) =>
+    SecureStore.setItemAsync(sanitizeKey(key), value),
+  removeItem: (key) =>
+    SecureStore.deleteItemAsync(sanitizeKey(key)),
+};
+
 const firebaseConfig = {
   apiKey: 'AIzaSyC41HJlrJlevfkq0Lqa3jv2i4ohHTUnQWw',
   authDomain: 'fb-expo-tif23.firebaseapp.com',
@@ -18,6 +28,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(SecureStore),
+  persistence: getReactNativePersistence(secureStorePersistence),
 });
 export { app, db, auth };
